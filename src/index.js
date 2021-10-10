61922b6298fcf61c8504ee5c26aa45bb6d82247b
tahir_ahmad <simanto.saikat234@gmail.com>
2021-10-10 18:30:21 +0600

    2nd commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Clock extends React.Component {
+    render() {
+        return (
+            <h1 className="heding">
+                <span> {new Date().toLocaleTimeString(this.props.locale)}</span>
+            </h1>
+        );
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Clock locale="bn-BD" />, document.getElementById('root'));
